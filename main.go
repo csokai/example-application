2152686f7d6c922c00318becefa87852f5239f04
csokai <51289176+csokai@users.noreply.github.com>
2021-05-21 17:48:29 +0200

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,8 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Hello World.")
+		fmt.Fprintln(w, "My message.")
 	})
 	log.Fatalf("error: %s", http.ListenAndServe(":8080", nil))
-	fmt.Fprintln(w, "My message.")
+	
 }
